d628e7192bc52657969f3db3b87836e14e9e8bd5
mmc444 <mahdibay.1391.gg@gmail.com>
2026-08-14 00:31:28 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const products = [
+    {id: 1, name: 'هنر دستی ۱', price: 100000},
+    {id: 2, name: 'هنر دستی ۲', price: 200000}
+];
+let cart = [];
+
+function add(id) { 
+    cart.push(products.find(p=>p.id===id)); 
+    alert('به سبد اضافه شد!'); 
+}
+function checkout() { 
+    alert('در حال انتقال به واتس‌اپ...'); 
+}
+// نمایش محصولات
+const grid = document.getElementById('products');
+products.forEach(p => {
+    grid.innerHTML += `<div class='card'><h4>${p.name}</h4><p>${p.price} تومان</p><button onclick='add(${p.id})'>افزودن</button></div>`;
+});
